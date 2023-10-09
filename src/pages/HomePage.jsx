46b1906b1c12a93412ce7297528472f65244a611
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-10-09 11:50:11 +1100

    feat: Add Home Page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,27 @@
+
+
+export default function HomePage(){
+
+    return(
+        <div>
+            <nav className="navBar">
+                <ul>
+                    <li>mx51</li>
+                    <li>Jobs</li>
+                </ul>
+            </nav>
+            <body>
+                {/* Search box component */}
+                {/* Search All Departments Drop List */}
+                {/* Search all location Drop List */}
+                {/* Job Listing Display */}
+
+            </body>
+            <footer>
+                <p>Porwerd by Greenhouse</p>
+                <p>Read our Privacy Policy</p>
+            </footer>
+
+        </div>
+    )
+}
