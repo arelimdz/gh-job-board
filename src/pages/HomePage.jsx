11a63fd3100cbbae8afc8f068762743bc8bfa54a
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-10-09 12:03:41 +1100

    feat: Add components holders for homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,27 +1,15 @@
 
-
 export default function HomePage(){
 
     return(
         <div>
-            <nav className="navBar">
-                <ul>
-                    <li>mx51</li>
-                    <li>Jobs</li>
-                </ul>
-            </nav>
-            <body>
-                {/* Search box component */}
-                {/* Search All Departments Drop List */}
-                {/* Search all location Drop List */}
-                {/* Job Listing Display */}
-
-            </body>
-            <footer>
-                <p>Porwerd by Greenhouse</p>
-                <p>Read our Privacy Policy</p>
-            </footer>
-
+            {/* Search box component */}
+            {/* Search All Departments Drop List */}
+            {/* Search all location Drop List */}
+            {/* Job Listing Display */}
+                {/* nav: to JobPage  */}
+                {/* box: Job title  */}
+                {/* box: Deparment | Location  */}
         </div>
     )
 }
